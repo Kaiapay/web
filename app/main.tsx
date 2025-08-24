3f22265d0ecf20876186b269ddb488ca7ab70d44
Hakko <47938592+dividenconquer@users.noreply.github.com>
2025-08-25 02:27:04 +0900

    feat: 🎸 update

--- app/main.tsx
+++ app/main.tsx
@@ -4,6 +4,12 @@ import { createRoot } from "react-dom/client";
 import "./app.css";
 import App from "./App";
 
+if (import.meta.env.VITE_APP_ENV !== "production") {
+  import("eruda").then((eruda) => {
+    eruda.default.init();
+  });
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
