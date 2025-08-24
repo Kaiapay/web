33248741e2b5bd4d77d5ce2fb7a553cc9a9cd130
Hakko <47938592+dividenconquer@users.noreply.github.com>
2025-08-24 20:18:09 +0900

    feat: 🎸 add dev api

--- orval.config.ts
+++ orval.config.ts
@@ -1,27 +1,28 @@
-import { defineConfig } from 'orval';
+import { defineConfig } from "orval";
 
 export default defineConfig({
   petstore: {
-    input: 'https://petstore.swagger.io/v2/swagger.json',
+    input: "https://dev-api.kaiapay.app/openapi.json",
     output: {
-      target: './app/generated/api.ts',
-      client: 'react-query',
+      target: "./app/generated/api.ts",
+      client: "react-query",
       override: {
         mutator: {
-          path: './app/lib/api-client.ts',
-          name: 'customInstance',
+          path: "./app/lib/api-client.ts",
+          name: "customInstance",
         },
         query: {
           useQuery: true,
           shouldExportQueryKey: true,
           useMutation: true,
           useInfinite: false,
-          useInfiniteQueryParam: 'pageParam',
-        }
+          useInfiniteQueryParam: "pageParam",
+        },
       },
     },
     hooks: {
-      afterAllFilesWrite: 'prettier --write',
+      afterAllFilesWrite:
+        "npx prettier --write --ignore-path .prettierignore ./app/generated/api.ts",
     },
   },
 });
