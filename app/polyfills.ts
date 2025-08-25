d5391b88319d4109e3dfa572dbca578b933467ce
Hakko <47938592+dividenconquer@users.noreply.github.com>
2025-08-25 22:30:38 +0900

    feat: 🎸 update

--- app/polyfills.ts
+++ app/polyfills.ts
@@ -1,17 +1,17 @@
-// import { Buffer } from "buffer";
+import { Buffer } from "buffer";
 
-// if (typeof window !== "undefined") {
-//   // @ts-ignore
-//   window.pBuffer = window.Buffer;
-//   window.Buffer = Buffer;
-//   window.global = window.global || window;
+if (typeof window !== "undefined") {
+  // @ts-ignore
+  window.pBuffer = window.Buffer;
+  window.Buffer = Buffer;
+  window.global = window.global || window;
 
-//   if (!window.process) {
-//     window.process = { env: {} } as any;
-//   }
-// }
+  if (!window.process) {
+    window.process = { env: {} } as any;
+  }
+}
 
-// // Also set up for Node.js environment
-// if (typeof global !== "undefined" && !global.Buffer) {
-//   global.Buffer = Buffer;
-// }
+// Also set up for Node.js environment
+if (typeof global !== "undefined" && !global.Buffer) {
+  global.Buffer = Buffer;
+}
