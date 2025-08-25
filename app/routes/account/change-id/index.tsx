7d728c3be97062ded011761f86b3372afbb384ce
Youngho Joo <creative@yonsei.ac.kr>
2025-08-26 00:41:43 +0900

    아이디 UI 개선

--- app/routes/account/change-id/index.tsx
+++ app/routes/account/change-id/index.tsx
@@ -95,7 +95,7 @@ export default function ChangeIdPage() {
             아이디
           </div>
           <div className="flex flex-row items-center">
-            <div className="font-normal text-[16px] leading-[22px] tracking-[-0.1px] text-white pb-1">
+            <div className="font-normal text-[16px] leading-[22px] tracking-[-0.1px] text-white/70 pr-0.5 pb-0.5">
               @
             </div>
             <input
